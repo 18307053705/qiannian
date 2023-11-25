249e4011cd540c1232423e426e7f1f39152da905
2596523458@qq.com <18307053705>
2023-11-25 17:29:58 +0800

    feat: 战斗系统优化

--- server/utils/scheduleCronstyleFn/index.js
+++ server/utils/scheduleCronstyleFn/index.js
@@ -32,10 +32,10 @@ const roleFn = require("../roleFn");
 // 每周1的1点1分30秒触发 ：'30 1 1 * * 1'
 
 const scheduleCronstyle = () => {
-    ActiveQueueG.openZhanChang();
-    ActiveQueueG.openWorldBoss();
-    ActiveQueueG.openCaiLingDong();
-    ActiveQueueG.openJinYindao();
+    // ActiveQueueG.openZhanChang();
+    // ActiveQueueG.openWorldBoss();
+    // ActiveQueueG.openCaiLingDong();
+    // ActiveQueueG.openJinYindao();
     // 每天19点 开启上古战场
     schedule.scheduleJob('0 0 19 * * *', () => {
         ActiveQueueG.openZhanChang();
