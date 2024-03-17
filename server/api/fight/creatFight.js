e822fdbe2fbf165d3d0f4aaf69d0b8080e69e7f2
2596523458@qq.com <18307053705>
2024-03-17 22:01:55 +0800

    feat: 战斗流量消耗优化

--- server/api/fight/creatFight.js
+++ server/api/fight/creatFight.js
@@ -25,8 +25,7 @@ module.exports = {
     res.send({
       code: 0,
       data: {
-        ...fightFn.getFightFormat(req, res),
-        // fightS: FightG.getFightGlobal(req, res)
+        ...fightFn.getFightFormat(req, res)
       }
     })
   },
